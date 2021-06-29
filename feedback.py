255607c19c44562a4897443747cb93ca8ddc30b2
emenella <emenella@student.42lyon.fr>
2021-06-29 05:20:54 +0200

    fonctionnel mais em peu faire mieux

--- feedback.py
+++ feedback.py
@@ -1,9 +1,11 @@
 #! /Users/emenella/.brew/bin/python3
 import requests
+import os
 from requests import adapters
 from requests.models import Response
 from requests.sessions import Session
 from bs4 import BeautifulSoup as bs
+from multiprocessing import Process
 
 
 def login(session: Session):
@@ -29,7 +31,7 @@ def logout(session: Session):
 	}
 	response = session.post('https://signin.intra.42.fr/users/sign_out',data=data)
 
-def get_project(session: Session, login: str):
+def get_project(session: Session, url: str):
 	headers = {
     'Connection': 'keep-alive',
     'Cache-Control': 'max-age=0',
@@ -46,24 +48,62 @@ def get_project(session: Session, login: str):
     'Accept-Language': 'fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7',
     'If-None-Match': 'W/"d632017c58c246eb0844bf6a409e37e5"',
 	}
-	response = session.get("https://profile.intra.42.fr/users/emenella", headers=headers)
+	response = session.get(url, headers=headers)
 	parser = bs(response.text, "html.parser")
 	links_with_text = []
 	for a in parser.find_all('span', {'class': 'marked-title'}):
 		for b in a.find_all('a'):
 			links_with_text.append(b.get('href'))
+	links_with_text = list(set(links_with_text))
 	return (links_with_text)
 
 def get_feedback(session: Session, url: str):
 	reponse = session.get(url)
-	
-	
-	
+	parser = bs(reponse.content, "html.parser")
+	coco = parser.select('.correction-comment-item')
+	brut = [com.text for com in coco]
+	feedback = []
+	for line in brut:
+		feedback.append(line.splitlines()[1])
+	for line in feedback:
+		line.strip('\n')
+	return(feedback)
+
+def get_profil_from_project(session: Session, url: str):
+	reponce = session.get(url)
+	parser = bs(reponce.content, "html.parser")
+	profil = []
+	for a in parser.find_all('a', {'data-tooltip-url': 'https://profile.intra.42.fr'}):
+		profil.append(a.get('href'))
+	return(profil)
+
+def boucle(session: Session, project: str, limit: int):
+	if (limit < 8):
+		limit += 1
+		feedback = get_feedback(session, project)
+		for f in feedback:
+			print(f)
+		profils = get_profil_from_project(session, project)
+		for p in profils:
+			explorer(session, p, limit)
+
+def explorer(session: Session, entrypoint: str, limit: int):
+	projects = get_project(session, entrypoint)
+	procs = []
+	for project in projects:
+		proc = Process(target=boucle, args=(session, project, limit))
+		procs.append(proc)
+		proc.start()
+	for proc in procs:
+		proc.join()
+
+
 def main():
-	user = "hjordan"
+	user = "https://profile.intra.42.fr/users/emenella"
 	session = requests.session()
 	login(session)
+	explorer(session, user, 0)
 	logout(session)
 
-
-main()
+if __name__ == "__main__":
+	main()
